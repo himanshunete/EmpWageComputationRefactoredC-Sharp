c03912b17e58ea1e3b8108c8c4e0d623ba3275af
himanshunete <himanshuneteh@gmail.com>
2021-02-04 15:25:21 +0530

    [himanshunete]Add CheckingTotalWage

--- EmpWageComputationUC7/Program.cs
+++ EmpWageComputationUC7/Program.cs
@@ -13,35 +13,45 @@ namespace EmpWageComputationUC7
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             //Constant
             int IS_PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 0;
-            int EMP_RATE_PER_HOUR = 20;
 
-            //Variables  
-            int empHrs = 0;
-            int empWage;
+            //Variable
+            int totalEmpWage = 0;
+            int empHrs;
+            int empWage = 0;
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
-            int workingTime = rand.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
-                Console.WriteLine("Employee is Present");
-                switch (workingTime)
+                Console.WriteLine(" Employee is Present ");
+                for (int day = 0; day <= NUM_OF_WORKING_DAYS; day++)
                 {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
+                    int workingTime = rand.Next(0, 2);
+                    switch (workingTime)
+                    {
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;
+                    Console.WriteLine(" Emp Daily Wage: " + empWage);
                 }
+                Console.WriteLine("Emp Total Wage: " + totalEmpWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Daily Wage: " + empWage);
-
+            else
+            {
+                Console.WriteLine(" Employee is Absent ");
+            }
 
 
         }
