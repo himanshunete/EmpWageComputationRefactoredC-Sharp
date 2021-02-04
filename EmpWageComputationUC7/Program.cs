a0299e96e1de873a0a4967d3426a9d77a23e36ae
himanshunete <himanshuneteh@gmail.com>
2021-02-04 15:30:31 +0530

    [himanshunete]Add UsingWhileLoop

--- EmpWageComputationUC7/Program.cs
+++ EmpWageComputationUC7/Program.cs
@@ -13,22 +13,28 @@ namespace EmpWageComputationUC7
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             //Constant
             int IS_PRESENT = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int NUM_OF_WORKING_DAYS = 20;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 0;
+            int EMP_RATE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
+            int MAX_NUM_OF_HOURS = 100;
 
             //Variable
             int totalEmpWage = 0;
-            int empHrs;
-            int empWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine(" Employee is Present ");
-                for (int day = 0; day <= NUM_OF_WORKING_DAYS; day++)
+                while (totalEmpHrs <= MAX_NUM_OF_HOURS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
                 {
+                    totalEmpHrs++;
+                    totalWorkingDays++;
+                    //Variables  
+                    int empHrs;
+                    int empWage;
                     int workingTime = rand.Next(0, 2);
                     switch (workingTime)
                     {
@@ -47,10 +53,6 @@ namespace EmpWageComputationUC7
                     Console.WriteLine(" Emp Daily Wage: " + empWage);
                 }
                 Console.WriteLine("Emp Total Wage: " + totalEmpWage);
-            }
-            else
-            {
-                Console.WriteLine(" Employee is Absent ");
             }
 
 
