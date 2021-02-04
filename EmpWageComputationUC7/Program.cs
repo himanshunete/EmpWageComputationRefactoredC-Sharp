a8d372f80d46aef8389b5077ae7fc583a6fb2e2a
himanshunete <himanshuneteh@gmail.com>
2021-02-04 15:15:04 +0530

    [himanshunete]Add CheckPartTimeWage

--- EmpWageComputationUC7/Program.cs
+++ EmpWageComputationUC7/Program.cs
@@ -13,17 +13,27 @@ namespace EmpWageComputationUC7
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             //Constant
             int IS_PRESENT = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 0;
             int EMP_RATE_PER_HOUR = 20;
 
             //Variables  
-            int empHrs;
+            int empHrs = 0;
             int empWage;
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
+            int workingTime = rand.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                if (workingTime == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                }
+                else if (workingTime == IS_PART_TIME)
+                {
+                    empHrs = 4;
+                }
             }
             else
             {
@@ -32,6 +42,7 @@ namespace EmpWageComputationUC7
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Daily Wage: " + empWage);
+
 
         }
     }
