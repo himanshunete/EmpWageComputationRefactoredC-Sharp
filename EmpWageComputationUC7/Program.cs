c82791967e913069002c28573732bbfc21a198a8
himanshunete <himanshuneteh@gmail.com>
2021-02-04 15:01:29 +0530

    [himanshunete]Add CheckAttendance

--- EmpWageComputationUC7/Program.cs
+++ EmpWageComputationUC7/Program.cs
@@ -4,9 +4,26 @@ namespace EmpWageComputationUC7
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            //Constant
+            int IS_PRESENT = 1;
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
