170dcccbd040e8a38d15aacba895e7b61e7b878a
himanshunete <himanshuneteh@gmail.com>
2021-02-04 15:08:34 +0530

    [himanshunete]Add CheckDailyWage

--- EmpWageComputationUC7/Program.cs
+++ EmpWageComputationUC7/Program.cs
@@ -13,16 +13,25 @@ namespace EmpWageComputationUC7
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             //Constant
             int IS_PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables  
+            int empHrs;
+            int empWage;
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Daily Wage: " + empWage);
 
         }
     }
