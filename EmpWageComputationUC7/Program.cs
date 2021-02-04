b56ece498ce5b1fad5726c5748180001c1217dc4
himanshunete <himanshuneteh@gmail.com>
2021-02-04 14:58:47 +0530

    [himanshunete]Add FirstCommit

---
+++ EmpWageComputationUC7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageComputationUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
